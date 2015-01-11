210d06039884b976a5c25952bcfc8d3d5bcd9a3a
Philipp Zedler <philipp@neue-musik.com>
2015-01-11 23:13:48 +0100

    A first version

---
+++ jquery.adjacent.js
@@ -0,0 +1,27 @@
+(function ($) {
+    /**
+     * MIT License
+     * https://github.com/philippze/jquery-adjacent/blob/master/LICENSE
+     **/
+    $.fn.adjacent = function (selector) {
+        var list = [];
+        var group;
+        var isAdjacent = false;
+        var first = true;
+        $(this).find(selector).each(function () {
+            if (first) {
+                group = [$(this)];
+                first = false;
+            } else if (isAdjacent) {
+                group.push($(this));
+            } else {
+                list.push(group);
+                group = [$(this)];
+            }
+            isAdjacent = $(this).next().is(selector);
+        });
+        list.push(group);
+        return list;
+    };
+    
+}(jQuery));
